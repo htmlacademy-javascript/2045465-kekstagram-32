6e74aaf484caba5b57c4ab3836f939f93cbab48a
Дементьева Инна <acorp.murka@gmail.com>
2024-07-18 23:39:14 +0300

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
+import './miniature.js';
+import {getPhotoMiniature} from './miniature.js';
 import {createPhotos} from './data.js';
-createPhotos();
+import {SIMILAR_PHOTO_COUNT} from './data.js';
+getPhotoMiniature(createPhotos(SIMILAR_PHOTO_COUNT));
